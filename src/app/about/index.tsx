75cb8bed8460a33d58bfe3c25a43db8fba2f0a0f
罗智毅 <924361501@qq.com>
2023-11-22 23:51:54 +0800

    chore: 优化

--- src/app/about/index.tsx
+++ src/app/about/index.tsx
@@ -1,3 +1,3 @@
 export default function About() {
-  return <>about pc</>;
+  return <>pc about</>;
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from 'react';
 import { Inter } from 'next/font/google';
 import UAParser from 'ua-parser-js';
+import '@/styles/tailwind.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
